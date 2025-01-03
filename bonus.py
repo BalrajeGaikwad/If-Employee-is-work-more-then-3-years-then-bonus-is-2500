6578f517f1fffff772b0cad17a8a0197ea4caf9b
BalrajeGaikwad <balrajegaikwad55@gmail.com>
2025-01-03 20:41:44 +0530

    if employee work more then 3 years then able to take bonus 2500

---
+++ bonus.py
@@ -0,0 +1,22 @@
+#The current year and the year in which the employee joined the organization are entered through the keyboard. 
+#If the number of years for which the employee has served the organization is greater than 3, then a bonus of Rs. 2500/- is given to the employee. 
+#If the years of service are not greater than 3, then the program should do nothing.
+
+"""!- current yaer
+!- year in which employee joined
+!- no of year> 3 --> 2500"""
+
+
+join_year=int(input("Enter the year in which employee was joined :--"))
+current_year=int(input("Enter the current year :-- "))
+
+total_year=current_year-join_year
+
+print(total_year)
+emp=0
+
+if(total_year>3):
+    emp+=2500
+    print(emp)
+else:
+    print("not able to take a bonus u need to complete atleast 3 years in organization")
